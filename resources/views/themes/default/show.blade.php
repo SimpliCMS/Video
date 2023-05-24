2eb047e5ddeaebac76c06ddee2e68e81d87c2087
TohurTV <joshwebb84@outlook.com>
2023-05-23 19:45:18 -0600

    fix

--- resources/views/themes/default/show.blade.php
+++ resources/views/themes/default/show.blade.php
@@ -8,13 +8,13 @@
 @endpush
 <div class="container">
     <div class="row">
-        <div class="col-12 col-lg-8 col-xl-9">
+        <div class="col-13 col-lg-9 col-xl-10">
             @include('video::partials.show.player')
             @include('video::partials.show.channel')
             @include('video::partials.show.description')
         </div>
         
-        <div class="col-12 col-lg-8 col-xl-9">
+        <div class="col-11 col-lg-7 col-xl-8">
             @include('comment::comments', ['type' => 'video', 'type_id' => $video->id, 'comments' => $video->comments])
         </div>
     </div>
