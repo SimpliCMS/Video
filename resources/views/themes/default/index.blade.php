18c9b480c4dce2cd916322eb3e5b4a4ebcef3d39
TohurTV <joshwebb84@outlook.com>
2023-06-25 23:10:20 -0600

    various fixes due to implmenting Bootstrap color modes

--- resources/views/themes/default/index.blade.php
+++ resources/views/themes/default/index.blade.php
@@ -15,7 +15,7 @@ Videos
                         @foreach($videos->sortByDesc('created_at') as $video)
                         @if ($video->is_active)
                         <div class="col-md-6 col-lg-4">
-                            <div class="card bg-light mb-3">
+                            <div class="card mb-3">
                                 <a href="{{ route('video.show', $video->id) }}"><img src="{{ $video->getMedia('service_image')[0]->getUrl() }}" class="card-img-top" alt="..."></a>
                                 <div class="card-body">
                                     <p class="card-text"><a href="{{ route('video.show', $video->id) }}">{{ Str::limit($video->name, 50) }}</a></p>
